978e975aaa28aa5fa2ecd132e2b9c32ec8f7cee7
engrajibulhasan <engrajibulhasan@gmail.com>
2022-10-02 17:14:54 +0600

    Got some tips from arafat vai

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import jsonData from "./demo.json";
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
-  const data = jsonData.resFormat;
-
-
+  const handleClick = (e, value) => {
+    console.log(e.name, value);
+  }
+  let domContent = []
   const resComplexFormat = {
     mnoRequestType: "",
     responseRecord: [
       {
         bts: {
-          band: "",
+          band: {
+            bandOne: "",
+            bandTwo: ""
+          },
+
           btsId: "",
           g3g4g: "",
           siteId: "",
@@ -23,7 +25,8 @@ function App() {
           cellName: "",
           latitude: "",
           direction: "",
-          longitude: ""
+          longitude: "",
+          veryLong: ["Five", "Three"]
         },
         imsi: "",
         btsId: "",
@@ -50,54 +53,116 @@ function App() {
       }
     ],
     numberofRecordsFound: "",
-    operator: ""
+    operator: "",
+    arafatVai: {
+      vai1: "",
+      vai2: ""
+    }
   }
 
-  const resEasyFormat = {
-    name: "",
-    age: "",
-    gender: ""
+
+  const smallObject = {
+    bts: {
+      band: "",
+      others: {
+        btsId: "",
+        g3g4g: "",
+        siteId: "",
+        btsType: "",
+        cgiEcgi: "",
+        cellCode: "",
+        cellName: "",
+        latitude: "",
+        direction: "",
+        longitude: ""
+      },
+      another: {
+        sisters: "",
+        others: "",
+        brothers: {
+          elder: "",
+          younger: ""
+        }
+      }
+    }
   }
+
+
 
   function isObject(o) {
     return o instanceof Object && o.constructor === Object;
   }
 
-  const objectArrayDifferentiate = (data) => {
+  const getKeyArray = (data) => {
     if (Array.isArray(data)) {
-      return data;
+      return Object.keys(data[0]);
     } else if (isObject(data)) {
       return Object.keys(data)
     }
     return data;
   }
 
-  const generateCheckbox = (resFormat) => {
+  const style = { color: { color: "red" } }
+
+  let content = ``;
+  let marginCounter = 0;
+
+
+
+  const generateCheckbox = (data, root = '_') => {
     // Checking Array/Object
-    let objToArray = objectArrayDifferentiate(resFormat);
+    let keyArray = getKeyArray(data);
 
     // Is Object/Array
-    if (Array.isArray(objToArray)) {
-      let objLen = objToArray.length;
+    if (Array.isArray(keyArray)) {
+      keyArray.sort()
+      let objLen = keyArray.length;
+
       for (let i = 0; i < objLen; i++) {
-        if (Array.isArray(objectArrayDifferentiate(resFormat[objToArray[i]]))) {
-          console.log(objToArray[i]);//Print Only
-          generateCheckbox(resFormat[objToArray[i]])
-        } else {
-          !Array.isArray(objectArrayDifferentiate(objToArray[i])) && console.log(objToArray[i]);//Print only
-          generateCheckbox(objToArray[i])
+        const key = keyArray[i];
+        let _root = root + '_' + key;
+        let nested = data[key]
+        console.log(key, '=', marginCounter)
+
+        // Creating Array
+        domContent.push({ value: key, label: key, children: '', name: key, margin: marginCounter, id: _root })
+
+        if (Array.isArray(nested))
+          nested = nested[0]
+
+        if (isObject(nested)) {
+          marginCounter++;
+
+          generateCheckbox(nested, _root)
         }
 
       }
     }
+    marginCounter--;
   }
 
-  generateCheckbox(resComplexFormat)
 
+  // Function Call
+  generateCheckbox(resComplexFormat);
 
+  // Create Markup
+  function createMarkup() {
+    return { __html: content };
+  }
+
+  console.log(domContent);
   return (
-    <div className="App">
+    <div className="container">
       <h1>Deeply nested Object</h1>
+
+      {
+        domContent?.length > 0 && domContent.map((item, index) => (
+          <div className={`left-${item.margin}`}>
+            <input type="checkbox" name={item.id} onChange={(e) => handleClick(e.target, 12)} /> <span>{item.name}</span>
+          </div>
+        )
+        )
+      }
     </div>
   );
 }
